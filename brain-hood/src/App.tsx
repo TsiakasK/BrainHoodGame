d2c1d9a49c15ceb5c3c771babb1418d0615e213d
Mathioudakis Thodoris <tmathioudakis@gmail.com>
2020-09-28 20:36:24 +0300

    Setting up react app skeleton

---
+++ brain-hood/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+import React from 'react';
+
+import tsiakoPhoto from './assets/7MAR2015.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={tsiakoPhoto} alt="logo" />
+        <p> Κομμένα τα γέλια ΜΑΛΑΚΑΚΟ! </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
